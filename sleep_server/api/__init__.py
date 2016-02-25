e6b1923e555317377bd95bdc467c53bf1b89fcde
rudolfix <marcin.rudolf@dev.army>
2016-02-25 13:48:13 +0100

    base config is loaded from Config class then updated from file defined in WAKEUPP_APP_CONFIG_FILE env var. test config options added

--- sleep_server/api/__init__.py
+++ sleep_server/api/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 app.config.from_object('api.config.Config')
+if os.environ.get('WAKEUPP_APP_CONFIG_FILE') is not None:
+    app.config.from_envvar('WAKEUPP_APP_CONFIG_FILE')
 
 import api.api
 import admin.admin
--- sleep_server/api/config.py
+++ sleep_server/api/config.py
@@ -1,5 +1,6 @@
 import base64
 
+
 class Config(object):
     CLIENT_ID = '3547ee3842f24500aded0f5a0afe11a5'
     CLIENT_SECRET = '9a97bf95ce2048c2a3df751a50367060'
@@ -8,7 +9,20 @@ class Config(object):
     CLIENT_CALLBACK_URL = 'wakeupapp://'
     AUTH_HEADER = 'Basic ' + str(base64.standard_b64encode((CLIENT_ID + ':' + CLIENT_SECRET).encode('ascii')), 'ascii')
     SPOTIFY_ACCOUNTS_ENDPOINT = 'https://accounts.spotify.com'
-    ADMIN_SPOTIFY_LOGIN_SCOPE ='playlist-read-private playlist-read-collaborative playlist-modify-private user-follow-read user-library-read user-read-private'
+    SPOTIFY_LOGIN_SCOPE = 'playlist-read-private playlist-read-collaborative playlist-modify-private user-follow-read user-library-read user-read-private'
     ADMIN_AUTH_COOKIE = 'spotify-auth'
-    HOST_NAME = 'dev.wakeupapp.com'
+    HOST_NAME = 'dev.wakeupapp.com:5000'
     USER_STORAGE_URI = '/home/vagrant/user_storage/'
+    TESTING = False
+    MOCKUP_MIN_PLAYLIST_GEN_SEC = 15
+    MOCKUP_MAX_PLAYLIST_GEN_SEC = 2*60
+    MAXIMUM_PLAYLIST_LENGTH = 80*60*1000
+    DEBUG = True
+
+
+class TestConfig(Config):
+    # USER_STORAGE_URI = super.USER_STORAGE_URI + '/../test_user_storage/'
+    TEST_REFRESH_TOKEN = 'AQBz4V96VzESfrk7NM0QGocj-2mNKfaIX-rAXa-ClZBbhAzkzX0xRYs07y6BFdOWK6Q1ak-5gx-FE1Q1BzoxFy3zupqQMGxlnIHUQt4qed8eY5oRyVqKB3zqx4A6bgzypf0' # put valid refresh token here. use /admin to obtain it
+    TESTING = True
+    MOCKUP_MIN_PLAYLIST_GEN_SEC = 10000
+    MOCKUP_MAX_PLAYLIST_GEN_SEC = 10001
