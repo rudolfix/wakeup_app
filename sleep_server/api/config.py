8d082fe4da1c1ac28a8c81913ad20b26797be4c3
rudolfix <marcin.rudolf@dev.army>
2016-02-26 12:12:25 +0100

    new client app callback scheme in config

--- sleep_server/api/config.py
+++ sleep_server/api/config.py
@@ -6,7 +6,7 @@ class Config(object):
     CLIENT_SECRET = '9a97bf95ce2048c2a3df751a50367060'
     ENCRYPTION_IV = '2d04dd0c8d0245c58db445ab24eb8df1'
     ENCRYPTION_KEY = '62b62c1bf34ea7b996cea01662cece92'
-    CLIENT_CALLBACK_URL = 'wakeupapp://'
+    CLIENT_CALLBACK_URL = 'luxury8wakeup://spotifylogincallback'
     AUTH_HEADER = 'Basic ' + str(base64.standard_b64encode((CLIENT_ID + ':' + CLIENT_SECRET).encode('ascii')), 'ascii')
     SPOTIFY_ACCOUNTS_ENDPOINT = 'https://accounts.spotify.com'
     SPOTIFY_LOGIN_SCOPE = 'playlist-read-private playlist-read-collaborative playlist-modify-private user-follow-read user-library-read user-read-private'
