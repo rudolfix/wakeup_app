50c03def3b9f781c335aab6cbb67e025192e1d57
rudolfix <marcin.rudolf@dev.army>
2016-03-25 19:24:51 +0100

    add mysql to vagrant config

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -5,12 +5,20 @@ apt-get -y install python3-dev
 apt-get -y install python3-pip
 apt-get -y install python3-scipy
 apt-get -y install git
+apt-get -y install libpcre3 libpcre3-dev
+apt-get -y install libmysqlclient-dev
 pip3 install -r /vagrant/requirements.txt
 #add development server to localhosts
 echo "0.0.0.0 dev.wakeupapp.com wakeupapp.com" >> /etc/hosts
 
 #prepare file user storage - we'll remove it when mongo is implemented
 mkdir /home/vagrant/user_storage/
+#install mysql
+apt-get -y install mysql-server # vagrant root password is oiqwj0a-a
+#set binding ip address https://help.ubuntu.com/12.04/serverguide/mysql.html: /etc/mysql/my.cnf to 0:0:0:0, port 3306
+#mysql -u root -p then CREATE USER 'dev'@'%';CREATE DATABASE music_graph;GRANT ALL ON music_graph.* TO 'dev'@'%';
+
+
 
 #configure python dev web server port forwarding from loopback to eth0
 #sysctl -w net.ipv4.conf.eth0.route_localnet=1
