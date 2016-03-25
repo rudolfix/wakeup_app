66c4de56ee66894564c05c98685e320034641446
rudolfix <marcin.rudolf@dev.army>
2016-03-25 19:22:35 +0100

    sql alchemy configured with local mysql db

--- sleep_server/api/__init__.py
+++ sleep_server/api/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object('api.config.Config')
 if os.environ.get('WAKEUPP_APP_CONFIG_FILE') is not None:
     app.config.from_envvar('WAKEUPP_APP_CONFIG_FILE')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from api.models import Artist
 
 import api.api
 import admin.admin
