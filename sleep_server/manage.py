6c98669f08d09b56265f2d5793aaf9c258deed45
rudolfix <marcin.rudolf@dev.army>
2016-04-07 22:30:57 +0200

    models (music graph schema) moved to server catalogue

--- sleep_server/manage.py
+++ sleep_server/manage.py
@@ -2,6 +2,7 @@ from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
 from api import app, db
+from server.models import *
 
 migrate = Migrate(app, db)
 manager = Manager(app)
