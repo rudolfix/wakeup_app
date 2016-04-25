2dab0b06b4817a5a2e9cd18737843659239751e8
rudolfix <marcin.rudolf@dev.army>
2016-04-25 03:01:51 +0200

    refresh and swap method merged to api.py, spotify.py deleted

--- sleep_server/api/api.py
+++ sleep_server/api/api.py
@@ -1,7 +1,10 @@
-from api import app, user_helper
-from api.exceptions import *
 from functools import wraps
 from flask import json, request
+
+from common import spotify_helper
+from api.user import User
+from api import app, user_helper
+from api.exceptions import *
 
 
 def check_user(f):
@@ -44,6 +47,41 @@ def set_playlist(user, playlist_type):
     user_helper.save_user(user)
     user_pl_meta = user_helper.create_user_playlist(user, playlist_type, desired_length)
     return json.jsonify(result=user_pl_meta)
+
+
+@app.route('/swap', methods=['POST', 'GET'])
+def swap():
+    auth_code = request.values.get('code')
+    if app.config['TESTING']:
+        token_data, status_code = spotify_helper.fake_token_for_code(app.config['TEST_REFRESH_TOKEN'])
+    else:
+        token_data, status_code = spotify_helper.token_for_code(auth_code, app.config['CLIENT_CALLBACK_URL'])
+    if status_code == 200:
+        user = user_helper.create_user(token_data)
+        # init gathering info on music user has
+        user_helper.gather_music_data(user)
+        # refresh token will contain encrypted spotify id
+        # todo: designs a better auth system with independent user id. spotify user id allows to recover user record after
+        # todo: app is reinstalled or user is logged again
+        token_data['refresh_token'] = User.encrypt_user_secret(user.spotify_id)
+    return json.jsonify(token_data), status_code
+
+
+@app.route('/refresh', methods=['POST', 'GET'])
+def refresh():
+    # descrypt refresh token, it will contain spotify_id (currently)
+    encrypted_rf = request.values.get('refresh_token')
+    spotify_id = User.decrypt_user_secret(encrypted_rf)
+    # user must exist
+    user = user_helper.load_user(spotify_id)
+    if user.is_new:
+        raise user_helper.UserDoesNotExist(request.values.get['refresh_token'])
+    # follow user procedure in SWAP
+    token_data, status_code = spotify_helper.refresh_token(user.spotify_refresh_token)
+    if status_code == 200:
+        user.update_refresh_token(token_data['access_token'], token_data['expires_in'])
+        user_helper.save_user(user)
+    return json.jsonify(token_data), status_code
 
 
 @app.errorhandler(ApiException)
