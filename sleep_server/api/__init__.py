115e2360d7b88daaa59e3aa59cfab89b8cbe9b62
rudolfix <marcin.rudolf@dev.army>
2016-04-25 03:19:30 +0200

    sqlalchemy removed from api, moved to server which is now separate flask app

--- sleep_server/api/__init__.py
+++ sleep_server/api/__init__.py
@@ -1,13 +1,11 @@
 import os
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('api.config.Config')
 if os.environ.get('WAKEUPP_APP_CONFIG_FILE') is not None:
     app.config.from_envvar('WAKEUPP_APP_CONFIG_FILE')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
 import api.api
+import admin.admin
