18f354707421fb0ddc99974b287199f80adf297a
rudolfix <marcin.rudolf@dev.army>
2016-05-24 11:28:45 +0200

    /api -added blueprint for admin

--- sleep_server/api/__init__.py
+++ sleep_server/api/__init__.py
@@ -3,9 +3,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+
 app.config.from_object('api.config.Config')
 if os.environ.get('WAKEUPP_APP_CONFIG_FILE') is not None:
     app.config.from_envvar('WAKEUPP_APP_CONFIG_FILE')
 
 import api.api
-import admin.admin
+from api.admin import admin
+app.register_blueprint(admin.admin_bp, url_prefix='/admin')
