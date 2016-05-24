d08de3846a78e99cfc750ca01c49732641cc1f4f
rudolfix <marcin.rudolf@dev.army>
2016-05-24 16:29:19 +0200

    tests: proper test of servers's get_best_playlist_id

--- sleep_server/tests/server_api_tests.py
+++ sleep_server/tests/server_api_tests.py
@@ -6,6 +6,7 @@ os.sys.path.insert(0, parentdir)
 
 from common import music_graph_client as mgc
 from common.user_base import UserBase
+from server import music_graph_helper as mgh, user_library as ul, server
 
 
 def apitest(f):
@@ -21,11 +22,34 @@ def apitest(f):
     return _wrap
 
 
+def test_mgc(user, user_unk):
+    apitest(mgc.get_library)(user)
+    apitest(mgc.get_library)(user_unk)
+    apitest(mgc.get_possible_playlists)(user)
+    apitest(mgc.get_possible_playlists)(user, 'fall_asleep')
+    apitest(mgc.create_playlist)(user, 'fall_asleep', 60*60*1000,828)
+
+
+def test_get_best_playlist_id(user, playlist_type):
+    server.start()
+    library = ul.load_library(user.spotify_id)
+    ul._delete_library(user.spotify_id, ul.UserLibraryProps)
+    lib_song_features, _ = mgh.load_user_library_song_features(library)
+    lib_song_features, _, _ = mgh.prepare_songs(lib_song_features, mgh.G.features_scaler)
+    top_sleepys = mgh.compute_sleep_genres(lib_song_features, library.artists)
+
+    for _ in range(10):
+        playlist_id = ul.get_best_playlist_id(playlist_type, library, top_sleepys, keep_n_last=4)
+        print('got plid %i' % playlist_id)
+
+    ul._delete_library(user.spotify_id, ul.UserLibraryProps)
+
+    for _ in range(len(top_sleepys)+5):
+        playlist_id = ul.get_best_playlist_id(playlist_type, library, top_sleepys, keep_n_last=len(top_sleepys)+1)
+        print('got plid %i' % playlist_id)
+
+
 user = UserBase.from_file('test_accounts/rudolfix-us.json')
 user_unk = UserBase.from_file('test_accounts/rudolfix-us.json')
 user_unk.spotify_id = 'unk'
-apitest(mgc.get_library)(user)
-apitest(mgc.get_library)(user_unk)
-apitest(mgc.get_possible_playlists)(user)
-apitest(mgc.get_possible_playlists)(user, 'fall_asleep')
-apitest(mgc.create_playlist)(user, 'fall_asleep', 60*60*1000,828)
+test_get_best_playlist_id(user, 'fall_asleep')
