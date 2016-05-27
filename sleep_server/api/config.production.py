6b5aca126f56f1e4647a9dd47c3ab2ace3dc1c14
rudolfix <marcin.rudolf@dev.army>
2016-05-27 21:09:02 +0200

    api config - removed unused entries

--- sleep_server/api/config.production.py
+++ sleep_server/api/config.production.py
@@ -1,11 +1,7 @@
 ENCRYPTION_IV = '2d04dd0c8d0245c58db445ab24eb8df1'
 ENCRYPTION_KEY = '62b62c1bf34ea7b996cea01662cece92'
 CLIENT_CALLBACK_URL = 'luxury8wakeup://spotifylogincallback'
-ADMIN_AUTH_COOKIE = 'spotify-auth'
 HOST_NAME = 'wakeupapp.dev.army'
 USER_STORAGE_URI = '/home/ubuntu/wakeupapp/user_storage/'
 TESTING = False
-MOCKUP_MIN_PLAYLIST_GEN_SEC = 15
-MOCKUP_MAX_PLAYLIST_GEN_SEC = 2*60
-MAXIMUM_PLAYLIST_LENGTH = 80*60*1000
 DEBUG = False
