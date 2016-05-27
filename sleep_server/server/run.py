2c8b3f304a9d3f690b2a706b0b6f9dd2bcd2ee02
rudolfix <marcin.rudolf@dev.army>
2016-05-27 21:03:19 +0200

    server: server init moved out of __main__ to work in uwsgi

--- sleep_server/server/run.py
+++ sleep_server/server/run.py
@@ -7,9 +7,9 @@ os.sys.path.insert(0,parentdir)
 from server import app
 from server import server
 
+# init logging
+server.init_logging()
+# start server
+server.start(start_mq=True)
 if __name__ == '__main__':
-    # init logging
-    server.init_logging()
-    # start server
-    server.start()
     app.run(app.config['HOST_NAME'], port=5001, debug=app.config['DEBUG'])
