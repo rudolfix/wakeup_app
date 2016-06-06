269ccfbb81aaba3a626ece1735eeca4a4378499a
rudolfix <marcin.rudolf@dev.army>
2016-06-06 18:25:28 +0200

    server: added sql alchemy pool recycle timeout, removed connection pool from fpika

--- sleep_server/server/config.py
+++ sleep_server/server/config.py
@@ -10,6 +10,7 @@ class Config(ConfigBase):
     # V91CRTEB0IFMAJBMB 120 but they disabled ;>
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'mysql://dev@localhost/music_graph_dev2'
+    SQLALCHEMY_POOL_RECYCLE = 60*5  # in seconds
     DEBUG = True
     USER_STORAGE_URI = '/home/vagrant/user_storage/'
     FLASK_PIKA_PARAMS = {
@@ -19,10 +20,11 @@ class Config(ConfigBase):
         # 'port': 5672,  # amqp server port
         # 'virtual_host': 'vhost'  # amqp vhost
     }
-    FLASK_PIKA_POOL_PARAMS = {
-        'pool_size': 8 + 2,  # 2 channels for in process consumers
-        'pool_recycle': 600
-    }
+    FLASK_PIKA_POOL_PARAMS = None  # it seems connection pool does not work correctly in fpika
+    # {
+    #     'pool_size': 8 + 2,  # 2 channels for in process consumers
+    #     'pool_recycle': 600
+    # }
     # logging
     LOG_FILE = '/var/log/sleep_server/server.log'
     LOG_LEVEL = logging.DEBUG
